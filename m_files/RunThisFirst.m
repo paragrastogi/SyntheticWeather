e9d0fb660e0b81dbe5eb34c2e173f7a32ea80e71
paragrastogi <rastogi.parag@gmail.com>
2017-11-27 17:37:54 +0900

    Nothing major - just different examples.

--- m_files/RunThisFirst.m
+++ m_files/RunThisFirst.m
@@ -1,13 +1,15 @@
 % An example run of the CreateSyntheticFiles function.
 
 % Path to source file.
-pathEPWfile = fullfile('..', 'm_data', 'GEN', 'GEN_IWEC.epw');
+% pathEPWfile = fullfile('..', 'm_data', 'GEN', 'GEN_IWEC.epw');
+pathEPWfile = 'E:\WeatherDataNew\HistoricalData\NYC\NYC_CPR_TMY-36.epw';
 
 % Path to folder where you want all the new stuff saved.
-path_save_fldr = fullfile('..', 'syn_data', 'GEN');
+% path_save_fldr = fullfile('..', 'syn_data', 'GEN');
+path_save_fldr = 'F:\WeatherData\SyntheticData\NYC\NYC_36';
 
 % Number of samples. Change this to get more/less samples.
-nboot = 10;
+nboot = 50;
 
 % Is recorded data present? Default is no. If this is present, then the
 % script will calculate a bunch of summary statistics. However, you must
@@ -19,17 +21,20 @@ recdata = false;
 % Setting this to true will generate two variants: rcp45 and rcp85,
 % corresponding to the RCP 4.5 and RCP 8.5 simulations from the IPCC
 % respectively.
-ccdata = false;
+ccdata = true;
+
+ccpath = 'F:\WeatherData\CCdata\NYC';
 
 % Random seed. If you use the same random seed for any run with the same
 % source data, the random samples produced will be exactly the same.
-randseed = 42;
+randseed = 1;
 
 % If this is not the first time you are running these files, the hourly
 % Fourier and SARIMA models might be stored. You can specify the path to
 % these here.
-% hrmdlfile = ''
-% fourierfile = ''
+hrmdlfile = 'F:\WeatherData\SyntheticData\NYC\NYC_36\FourierFits_NYC_CPR_TMY-36.mat';
+fourierfile = 'F:\WeatherData\SyntheticData\NYC\NYC_36\HourMdls_NYC_CPR_TMY-36.mat';
 
 CreateSyntheticFiles(pathEPWfile, path_save_fldr, nboot, recdata, ...
-    ccdata, 'randseed', randseed)
+    ccdata, 'randseed', randseed, 'hrmdlfile', hrmdlfile, ...
+    'fourierfile', fourierfile, 'ccpath', ccpath)
--- m_files/RunThisForIndiFiles.m
+++ m_files/RunThisForIndiFiles.m
@@ -1,16 +1,23 @@
-outfolder = fullfile('..', 'syn_data','GEN');
-synfile = fullfile('..', 'syn_data','GEN', 'GEN_IWEC_Syn.mat');
-srcEPWfile = fullfile('..', 'm_data', 'GEN', 'GEN_IWEC.epw');
+% outfolder = fullfile('..', 'syn_data','NYC');
+outfolder = 'F:\WeatherData\SyntheticData\NYC\NYC_36';
+
+
+% synfile = fullfile('..', 'syn_data','NYC', 'NYC_IWEC_Syn.mat');
+
+% srcEPWfile = fullfile('..', 'm_data', 'NYC', 'NYC_IWEC.epw');
+srcEPWfile = 'E:\WeatherDataNew\HistoricalData\NYC\NYC_CPR_TMY-36.epw';
 
 % Delete the existing files that match the names of the new synthetic
 % files. Use this power wisely.
 destroyer = false;
 
-SMY_Create('GEN', synfile, outfolder, ...
-    'sublabel', 50, 'scenario', 'syn', 'srcEPWfile', srcEPWfile)
+% SMY_Create('NYC', synfile, outfolder, ...
+%     'sublabel', 50, 'scenario', 'syn', 'srcEPWfile', srcEPWfile)
 
-% SMY_Create('GEN', outfolder, synfile, ...
+% synfile = 'F:\WeatherData\SyntheticData\NYC\NYC_36\NYC_CPR_TMY-36_rcp85.mat';
+% SMY_Create('NYC', synfile, outfolder, ...
 %     'sublabel', 50, 'scenario', 'rcp85', 'srcEPWfile', srcEPWfile)
-% 
-% SMY_Create('GEN', outfolder, synfile, ...
-%     'sublabel', 50, 'scenario', 'rcp45', 'srcEPWfile', srcEPWfile)
+
+synfile = 'F:\WeatherData\SyntheticData\NYC\NYC_36\NYC_CPR_TMY-36_rcp45.mat';
+SMY_Create('NYC', synfile, outfolder, ...
+    'sublabel', 50, 'scenario', 'rcp45', 'srcEPWfile', srcEPWfile)
